0e3ccc057801656fa44c9e0abba56b21d763c72d
Rey Sajju <96593440+Reysajju@users.noreply.github.com>
2026-05-13 22:33:35 +0500

    Add files via upload

--- src/components/landing/LandingPage.jsx
+++ src/components/landing/LandingPage.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 'use client';
 import React from "react";
 import Hero from "./Hero";
@@ -27,33 +26,3 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-=======
-'use client';
-import React from "react";
-import Hero from "./Hero";
-import Covenant from "./Covenant";
-import Trident from "./Trident";
-import Author from "./Author";
-import Reviews from "./Reviews";
-import FinalReconquest from "./FinalReconquest";
-import AudioToggle from "./AudioToggle";
-
-const LandingPage = () => {
-  return (
-    <main
-      className="relative bg-[#060a0e] text-[#F0E6D2] overflow-hidden"
-      data-testid="landing-page"
-    >
-      <AudioToggle />
-      <Hero />
-      <Covenant />
-      <Trident />
-      <Author />
-      <Reviews />
-      <FinalReconquest />
-    </main>
-  );
-};
-
-export default LandingPage;
->>>>>>> Stashed changes
